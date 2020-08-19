7147ca259e2a77917a231a08bcdb00b450b1ca71
JeroenDM <jeroendemaeyer@live.be>
2020-08-19 20:28:26 +0200

    add new case with tilted y-plane to python tutorial

--- doc/ompl_constrained_planning/scripts/ompl_constraint_planning_example.py
+++ doc/ompl_constrained_planning/scripts/ompl_constraint_planning_example.py
@@ -55,10 +55,10 @@ COLOR_GREEN = std_msgs.msg.ColorRGBA(0.0, 1.0, 0.0, 1.0)
 COLOR_TRANSLUCENT = std_msgs.msg.ColorRGBA(0.0, 0.0, 0.0, 0.5)
 
 
-## BEGIN_SUB_TUTORIAL setup
+# BEGIN_SUB_TUTORIAL setup
 ##
-## Setup a RobotCommander and a MoveGroupCommander, see "move_group_python_interface_tutorial" for more details.
-## Everything is wrappen a a class so we can easily reuse the different components.
+# Setup a RobotCommander and a MoveGroupCommander, see "move_group_python_interface_tutorial" for more details.
+# Everything is wrappen a a class so we can easily reuse the different components.
 ##
 class ConstrainedPlanningTutorial(object):
     def __init__(self, group_name="panda_arm"):
@@ -81,11 +81,11 @@ class ConstrainedPlanningTutorial(object):
         self.ee_link = self.move_group.get_end_effector_link()
         self.obstacle_name = "obstacle"
 
-    ## END_SUB_TUTORIAL
+    # END_SUB_TUTORIAL
 
-    ## BEGIN_SUB_TUTORIAL start_state
-    ## Instead of using the current robot state as start state, we use a fixed state for the panda robot defined in its `srdf` config file.
-    ## The `get_named_target_values` returns a dictionary with joint names and values for the "ready" position.
+    # BEGIN_SUB_TUTORIAL start_state
+    # Instead of using the current robot state as start state, we use a fixed state for the panda robot defined in its `srdf` config file.
+    # The `get_named_target_values` returns a dictionary with joint names and values for the "ready" position.
     def create_start_state(self):
         """ Create a RobotState message from a named joint target for this robot. """
         ready = self.move_group.get_named_target_values("ready")
@@ -102,14 +102,15 @@ class ConstrainedPlanningTutorial(object):
 
         return state
 
-    ## END_SUB_TUTORIAL
+    # END_SUB_TUTORIAL
 
-    ## BEGIN_SUB_TUTORIAL pose_goal
+    # BEGIN_SUB_TUTORIAL pose_goal
     ##
-    ## To keep things simple, we use the current end-effector pose to quickly create a reasonable goal.
-    ## We also visualize the start and goal position of the end-effector in Rviz with a simple sphere.
-    ## We assume that when the `demo.launch` file for the panda robot is launched, the robot is in the "ready" position.
+    # To keep things simple, we use the current end-effector pose to quickly create a reasonable goal.
+    # We also visualize the start and goal position of the end-effector in Rviz with a simple sphere.
+    # We assume that when the `demo.launch` file for the panda robot is launched, the robot is in the "ready" position.
     def create_pose_goal(self):
+        self.move_group.clear_pose_targets()
         pose = self.move_group.get_current_pose()
 
         self.display_sphere(pose.pose, color=COLOR_RED)
@@ -121,9 +122,23 @@ class ConstrainedPlanningTutorial(object):
 
         return pose
 
-    ## END_SUB_TUTORIAL
+    def create_pose_goal_2(self):
+        self.move_group.clear_pose_targets()
+        pose = self.move_group.get_current_pose()
 
-    ## BEGIN_SUB_TUTORIAL ori_con
+        self.display_sphere(pose.pose, color=COLOR_RED)
+
+        pose.pose.position.x += 0.2
+        pose.pose.position.y += 0.3
+        pose.pose.position.z -= 0.3
+
+        self.display_sphere(pose.pose)
+
+        return pose
+
+    # END_SUB_TUTORIAL
+
+    # BEGIN_SUB_TUTORIAL ori_con
     def create_orientation_constraints(self):
         # use the orientation of the current robot pose as a nominal orientation
         # to center the constraints around.
@@ -140,9 +155,9 @@ class ConstrainedPlanningTutorial(object):
 
         return ocm
 
-    ## END_SUB_TUTORIAL
+    # END_SUB_TUTORIAL
 
-    ## BEGIN_SUB_TUTORIAL pos_con
+    # BEGIN_SUB_TUTORIAL pos_con
     def create_position_constraints(self):
         pcm = moveit_msgs.msg.PositionConstraint()
         pcm.header.frame_id = self.ref_link
@@ -153,8 +168,10 @@ class ConstrainedPlanningTutorial(object):
         cbox.dimensions = [0.1, 0.3, 0.5]
         pcm.constraint_region.primitives.append(cbox)
 
+        current_pose = self.move_group.get_current_pose()
+
         cbox_pose = geometry_msgs.msg.Pose()
-        cbox_pose.position.x = 0.3
+        cbox_pose.position.x = current_pose.pose.position.x
         cbox_pose.position.y = 0.1
         cbox_pose.position.z = 0.45
         cbox_pose.orientation.w = 1.0
@@ -165,10 +182,39 @@ class ConstrainedPlanningTutorial(object):
 
         return pcm
 
-    ## END_SUB_TUTORIAL
+    def create_position_constraints_2(self):
+        pcm = moveit_msgs.msg.PositionConstraint()
+        pcm.header.frame_id = self.ref_link
+        pcm.link_name = self.ee_link
 
-    ## BEGIN_SUB_TUTORIAL solve
-    ## We add a simple solve function that we can use to quickly try solving a problem for different path constraints.
+        cbox = shape_msgs.msg.SolidPrimitive()
+        cbox.type = shape_msgs.msg.SolidPrimitive.BOX
+        cbox.dimensions = [1.0, 0.001, 1.0]
+        pcm.constraint_region.primitives.append(cbox)
+
+        current_pose = self.move_group.get_current_pose()
+
+        cbox_pose = geometry_msgs.msg.Pose()
+        cbox_pose.position.x = current_pose.pose.position.x
+        cbox_pose.position.y = current_pose.pose.position.y
+        cbox_pose.position.z = current_pose.pose.position.z
+
+        quat = quaternion_from_euler(pi/4, 0, 0)
+        cbox_pose.orientation.x = quat[0]
+        cbox_pose.orientation.y = quat[1]
+        cbox_pose.orientation.z = quat[2]
+        cbox_pose.orientation.w = quat[3]
+        pcm.constraint_region.primitive_poses.append(cbox_pose)
+
+        # display the constraints in rviz
+        self.display_box(cbox_pose, cbox.dimensions)
+
+        return pcm
+
+    # END_SUB_TUTORIAL
+
+    # BEGIN_SUB_TUTORIAL solve
+    # We add a simple solve function that we can use to quickly try solving a problem for different path constraints.
     def solve(self, start_state, pose_goal, path_constraints):
         self.move_group.set_start_state(start_state)
         self.move_group.set_pose_target(pose_goal)
@@ -180,10 +226,10 @@ class ConstrainedPlanningTutorial(object):
         # The move_group node should autmatically visualize the solution in Rviz if a path is found.
         self.move_group.plan()
 
-        ## Clear the path constraints for our next experiment
+        # Clear the path constraints for our next experiment
         self.move_group.clear_path_constraints()
 
-    ## END_SUB_TUTORIAL
+    # END_SUB_TUTORIAL
 
     def display_box(self, pose, dimensions):
         """ Utility function to visualize position constraints. """
@@ -245,67 +291,87 @@ class ConstrainedPlanningTutorial(object):
 
 
 def run_tutorial():
-    ## BEGIN_SUB_TUTORIAL main
+    # BEGIN_SUB_TUTORIAL main
     ##
-    ## Now we can use the different pieces we just defined to solve some planning problems!
-    ## First create an instance and get a start_state and pose_goal
+    # Now we can use the different pieces we just defined to solve some planning problems!
+    # First create an instance and get a start_state and pose_goal
     tutorial = ConstrainedPlanningTutorial()
     start_state = tutorial.create_start_state()
     pose_goal = tutorial.create_pose_goal()
 
-    ## Now let's try the orientation constraints!
+    # Now let's try the orientation constraints!
     ocm = tutorial.create_orientation_constraints()
 
-    ## We need two wrap the constraints in a generic `Constraints` message.
+    # We need two wrap the constraints in a generic `Constraints` message.
     path_constraints = moveit_msgs.msg.Constraints()
     path_constraints.orientation_constraints.append(ocm)
 
     # tutorial.add_obstacle()
     # rospy.sleep(0.1)
 
-    ## Call our simple solve function and look at Rviz to see the result.
+    # Call our simple solve function and look at Rviz to see the result.
     tutorial.solve(start_state, pose_goal, path_constraints)
 
-    ## Now wait for the user (you) to press enter before doing trying the position constraints.
+    # Now wait for the user(you) to press enter before doing trying the position constraints.
     print("============ Press enter to continue with the second planning problem.")
     input()
 
     pcm = tutorial.create_position_constraints()
+
     path_constraints = moveit_msgs.msg.Constraints()
     path_constraints.orientation_constraints = []
     path_constraints.position_constraints.append(pcm)
+
+    path_constraints.name = "use_equality_constraints"
+
     tutorial.solve(start_state, pose_goal, path_constraints)
-    ## END_SUB_TUTORIAL
+
+    print("============ Press enter to continue with the second planning problem.")
+    input()
+    tutorial.remove_all_markers()
+    pose_goal = tutorial.create_pose_goal_2()
+
+    pcm = tutorial.create_position_constraints_2()
+
+    path_constraints = moveit_msgs.msg.Constraints()
+    path_constraints.orientation_constraints = []
+    path_constraints.position_constraints.append(pcm)
+
+    path_constraints.name = "use_equality_constraints"
+
+    tutorial.solve(start_state, pose_goal, path_constraints)
+
+    # END_SUB_TUTORIAL
 
 
 if __name__ == "__main__":
     run_tutorial()
 
-## BEGIN_TUTORIAL
+# BEGIN_TUTORIAL
 ##
-## Setup
-## *****
-## CALL_SUB_TUTORIAL setup
+# Setup
+# *****
+# CALL_SUB_TUTORIAL setup
 ###
-## Create a planning problem
-## ***************************
-## CALL_SUB_TUTORIAL start_state
-## CALL_SUB_TUTORIAL pose_goal
+# Create a planning problem
+# ***************************
+# CALL_SUB_TUTORIAL start_state
+# CALL_SUB_TUTORIAL pose_goal
 ##
-## Create orientation constraints
-## ******************************
-## CALL_SUB_TUTORIAL ori_con
+# Create orientation constraints
+# ******************************
+# CALL_SUB_TUTORIAL ori_con
 ##
-## Create position constraints
-## ***************************
-## CALL_SUB_TUTORIAL pos_con
+# Create position constraints
+# ***************************
+# CALL_SUB_TUTORIAL pos_con
 ##
-## Create a simple solve function
-## ******************************
-## CALL_SUB_TUTORIAL solve
+# Create a simple solve function
+# ******************************
+# CALL_SUB_TUTORIAL solve
 ##
-## Finally, solve a planning problem!
-## **********************************
-## CALL_SUB_TUTORIAL main
+# Finally, solve a planning problem!
+# **********************************
+# CALL_SUB_TUTORIAL main
 ##
-## END_TUTORIAL
+# END_TUTORIAL
